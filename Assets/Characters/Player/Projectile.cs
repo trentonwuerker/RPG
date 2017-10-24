3452aa3d597b4e787d1e5a63476dabc562b3732e
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-10-24 14:50:54 -0600

    added cliff terrain

---
+++ Assets/Characters/Player/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float projectileSpeed;
+	public float damageCaused = 10f;
+
+	void OnCollisionEnter(Collision collision) {
+		Component damagableComponent = collision.gameObject.GetComponent (typeof(IDamagable));
+
+		if (damagableComponent) {
+			(damagableComponent as IDamagable).TakeDamage (damageCaused);
+		}
+		Destroy (gameObject, 0.01f);
+ 	}
+
+}
