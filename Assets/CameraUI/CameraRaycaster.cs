f88ca92956aa3613377e3723358c7861c1b63ea2
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-10-27 12:17:24 -0600

    started energy mechanic

--- Assets/CameraUI/CameraRaycaster.cs
+++ Assets/CameraUI/CameraRaycaster.cs
@@ -18,8 +18,10 @@ namespace RPG.CameraUI
         public event OnCursorLayerChange notifyLayerChangeObservers; // instantiate an observer set
 
         public delegate void OnClickPriorityLayer(RaycastHit raycastHit, int layerHit); // declare new delegate type
-        public event OnClickPriorityLayer notifyMouseClickObservers; // instantiate an observer set
+        public event OnClickPriorityLayer notifyMouseClickObservers;
 
+        public delegate void OnRightClick(RaycastHit raycastHit, int layerHit); // declare new delegate type
+        public event OnClickPriorityLayer notifyRightClickObservers;
 
         void Update()
         {
@@ -49,6 +51,11 @@ namespace RPG.CameraUI
             if (Input.GetMouseButton(0))
             {
                 notifyMouseClickObservers(priorityHit.Value, layerHit);
+            }
+            if (Input.GetMouseButtonDown(1))
+            {
+                notifyRightClickObservers(priorityHit.Value, layerHit);
+
             }
         }
 
--- Assets/Energy.cs
+++ Assets/Energy.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using RPG.CameraUI;
+using System;
 
 namespace RPG.Characters
 {
@@ -9,18 +11,41 @@ namespace RPG.Characters
     {
         [SerializeField] RawImage energyBar;
         [SerializeField] float maxEnergyPoints = 100f;
+        [SerializeField] float pointsPerHit = 10f;
 
         float currentEnergyPoints;
-        // Use this for initialization
+        CameraRaycaster cameraRaycaster;
+
         void Start()
         {
             currentEnergyPoints = maxEnergyPoints;
+            cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+            cameraRaycaster.notifyRightClickObservers += ProcessRightClick;
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        void ProcessRightClick(RaycastHit raycastHit, int layerHit)
+        {
+            float newEnergyPoints = currentEnergyPoints - pointsPerHit;
+            currentEnergyPoints = Mathf.Clamp(newEnergyPoints, 0, maxEnergyPoints);
+
+            UpdateEnergyBar();
+        }
+
+        private void UpdateEnergyBar()
+        {
+            float xValue = -(EnergyAsPercent() / 2f) - 0.5f;
+            energyBar.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+        }
+
+        private float EnergyAsPercent()
+        {
+            return currentEnergyPoints / maxEnergyPoints;
         }
     }
 }
